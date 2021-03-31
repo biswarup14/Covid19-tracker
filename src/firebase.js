fcd64d312debbc919778344722cd40e6bdc09c26
Biswarup Bhattacharjee <biswanathbhattacharya@Biswanaths-MacBook-Pro.local>
2021-03-31 14:13:18 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+
+ firebase.initializeApp( {
+    apiKey: "AIzaSyAGC9b3b_-V2QZg4n8aTmfi9bjfiq4vVm0",
+    authDomain: "covid-19-tracker-app-50b1c.firebaseapp.com",
+    databaseURL: "https://covid-19-tracker-app-50b1c-default-rtdb.firebaseio.com",
+    projectId: "covid-19-tracker-app-50b1c",
+    storageBucket: "covid-19-tracker-app-50b1c.appspot.com",
+    messagingSenderId: "379122171786",
+    appId: "1:379122171786:web:9720d7805d6a4e6ca01273"
+  });
